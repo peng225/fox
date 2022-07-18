4d107d3d98e9afbd3ee364a8cc1aa17d93233015
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-07-18 20:05:27 +0900

    kubebuilder create webhook
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- main.go
+++ main.go
@@ -96,6 +96,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "PVCBackup")
 		os.Exit(1)
 	}
+	if err = (&foxv1alpha1.PVCBackup{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "PVCBackup")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
