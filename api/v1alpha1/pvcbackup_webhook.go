f8669d93aba3cb301d5cf1b75139d57caa0e57ef
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-07-19 21:37:17 +0900

    add default value for backup status.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- api/v1alpha1/pvcbackup_webhook.go
+++ api/v1alpha1/pvcbackup_webhook.go
@@ -42,7 +42,7 @@ var _ webhook.Defaulter = &PVCBackup{}
 func (r *PVCBackup) Default() {
 	pvcbackuplog.Info("default", "name", r.Name)
 
-	// TODO(user): fill in your defaulting logic.
+	r.Status.BackupStatus = BackupNotStarted
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
